58073230584f7ab1532e4f5dbde7cc89ed3896f8
Benis Tambe <will.jenifer@yahoo.com>
2021-08-18 12:00:32 -0400

    commit tree d03837052eeed78d0abcb45841eaf91d923f6770

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your Circle class here
+
+const pi = Math.PI;
+class Circle {
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    get diameter(){
+        return (this.radius *2);
+    }
+    set diameter(newDiameter){
+        this.radius = newDiameter/2;
+    }
+    get circumference(){
+        return ( this.radius * 2 ) * pi
+    }
+    set circumference(newCirc){
+        this.radius = newCirc / ( pi * 2 )
+    }
+    get area(){
+        return ((this.radius **2) * pi)
+    }
+    set area(newArea){
+        this.radius = Math.sqrt( newArea / pi )
+    }
+}
